868f0b745ab854b2677121ea68c55b5130b7b592
jonathan-crepeau <jon.crepeau@gmail.com>
2022-06-20 21:24:35 -0700

    Part 1 completed

---
+++ js/main.js
@@ -0,0 +1,29 @@
+console.log("Fear is the mind killer");
+
+// PART 1 ----------------------
+
+// 1. Remove the highlight class from #box2 (use the .removeClass() method)
+
+$("#box2").removeClass("highlight");
+
+// 2. Select the anchor within #box3. Change the HTML to 'jQuery!'
+
+$("#box3").html("<p>jQuery!</p>");
+
+// 3. Add the .highlight class to #box4 (use the .addClass() method)
+
+$("#box4").addClass("highlight");
+
+// 4. Make the text color of the strong tag within #box2 green (use the .css() method)
+
+$("#box2 strong").css("color", "green");
+
+// PART 2 ----------------------
+
+// 1. When the anchor within #box3 is clicked
+// a) Slide toggle #box4
+// b) Fade out #box1 slowly
+
+// 1. When the h2 is clicked on
+// a) Slide up the h2
+// b) Add the snazzy class to the h1
